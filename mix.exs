2c0095e21f3243c3d00b14b4bd72dd14ff7e8fad
D <dannnylo@gmail.com>
2018-05-08 09:14:02 -0300

    Package

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule TesseractOcr.MixProject do
       elixir: "~> 1.6",
       description: "A wrapper for Tesseract OCR",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/dannnylo/tesseract-ocr-elixir"
     ]
   end
 
@@ -21,9 +23,14 @@ defmodule TesseractOcr.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      maintainers: ["Danilo Jeremias da Silva"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/dannnylo/tesseract-ocr-elixir"}
     ]
   end
 end
