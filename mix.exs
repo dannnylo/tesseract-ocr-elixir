63a0af130508e93325685260b1732d13c7ccbb62
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2019-07-30 08:15:00 -0300

    Bump Version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TesseractOcr.MixProject do
   def project do
     [
       app: :tesseract_ocr,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       description: "A wrapper for Tesseract OCR",
       start_permanent: Mix.env() == :prod,
